420afa5e269c0c498df3f3c6ee5321d7612fe418
Henrique Mamede <henrique.mamede13@hotmail.com>
2019-08-08 14:35:32 -0300

    atualizar configurações do projeto

--- tekla/tekla/settings.py
+++ tekla/tekla/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tekla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'template1',
+        'USER': 'postgres',
+        'PASSWORD': '2945m@n$68#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
