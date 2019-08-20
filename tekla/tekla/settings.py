68c157359fc40d0ce44a61d087bf3f62551880fd
Henrique Mamede <henrique.mamede13@hotmail.com>
2019-08-20 17:30:27 -0300

    configurar heroku

--- tekla/tekla/settings.py
+++ tekla/tekla/settings.py
@@ -119,8 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-django_heroku.settings(locals())
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -132,3 +130,4 @@ AUTH_USER_MODEL = 'teklando.Voluntario'
 LOGIN_REDIRECT_URL = 'voluntario'
 LOGOUT_REDIRECT_URL = 'index'
 
+django_heroku.settings(locals())
